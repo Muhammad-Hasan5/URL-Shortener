f8166467019142b8c03bf5b5db287e94814fb646
Muhammad Hasan <hasanamir8901@gmail.com>
2026-04-18 05:26:22 +0500

    mount the router on app

--- dist/app.js
+++ dist/app.js
@@ -1,7 +1,9 @@
 import express from "express";
+import router from "./routes/url.route.js";
 const app = express();
 app.use(express.json({ limit: "5mb" }));
 app.use(express.urlencoded({ limit: "5mb", extended: true }));
+app.use("/", router);
 app.get("/", (req, res) => {
     res.send("hello world");
 });
--- src/app.ts
+++ src/app.ts
@@ -1,9 +1,12 @@
 import express from "express";
+import router from "./routes/url.route.js";
 
 const app: express.Application = express();
 
 app.use(express.json({limit: "5mb"}))
 app.use(express.urlencoded({limit: "5mb", extended: true}))
+
+app.use("/", router)
 
 app.get("/", (req, res) => {
     res.send("hello world")
