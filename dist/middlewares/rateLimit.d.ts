666e6c248a2463b354c901f7354c6adb49bbbb80
Muhammad Hasan <hasanamir8901@gmail.com>
2026-05-07 04:51:44 +0500

    add: rate limit middleware

---
+++ dist/middlewares/rateLimit.d.ts
@@ -0,0 +1,4 @@
+import { type RateLimitRequestHandler } from "express-rate-limit";
+declare const limiter: RateLimitRequestHandler;
+export default limiter;
+//# sourceMappingURL=rateLimit.d.ts.map
---
+++ dist/middlewares/rateLimit.js
@@ -0,0 +1,18 @@
+process.loadEnvFile();
+import { rateLimit } from "express-rate-limit";
+import redis from "../config/redis/index.redis.js";
+import { RedisStore } from "rate-limit-redis";
+const windowMs = Number(process.env.RATE_LIMIT_WINDOWSMS);
+const maxReq = Number(process.env.RATE_LIMIT_MAX_REQUESTS);
+/* express-rate-limt => sliding window algo */
+const limiter = rateLimit({
+    windowMs,
+    limit: maxReq,
+    standardHeaders: true,
+    legacyHeaders: false,
+    store: new RedisStore({
+        sendCommand: (command, ...args) => redis.call(command, ...args),
+    }),
+});
+export default limiter;
+//# sourceMappingURL=rateLimit.js.map
