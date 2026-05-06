f29051b76f211e5a091c3aba82df12fe76456059
Muhammad Hasan <hasanamir8901@gmail.com>
2026-05-07 04:55:20 +0500

    add: circuit breaker to cache logic

---
+++ src/utils/cache_circuit_breaker/cacheCircuitBreaker.ts
@@ -0,0 +1,6 @@
+import createBreaker from "./index.js";
+import { set, get } from "../../config/redis/cache.redis.js";
+
+// creating breakers
+export const setBreaker = createBreaker(set);
+export const getBreaker = createBreaker(get);
