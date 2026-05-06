70ccfdba4647c9b5883cf4e3d7c67a793af5b207
Muhammad Hasan <hasanamir8901@gmail.com>
2026-05-07 00:26:52 +0500

    add: types for new database records and method returning types

--- dist/repositories/url.repository.d.ts
+++ dist/repositories/url.repository.d.ts
@@ -1,7 +1,11 @@
-export declare const saveToDB: (newRecord: {
+import type { QueryResult } from "pg";
+type newRecordType = {
     id: string;
     shortCode: string;
     longURL: string;
-}) => Promise<import("pg").QueryResult<any> | undefined>;
-export declare const getFromDB: (shortCode: string) => Promise<import("pg").QueryResult<any> | undefined>;
+};
+type shortCodeType = string;
+export declare const saveToDB: (newRecord: newRecordType) => Promise<QueryResult<any> | undefined>;
+export declare const getFromDB: (shortCode: shortCodeType) => Promise<QueryResult<any> | undefined>;
+export {};
 //# sourceMappingURL=url.repository.d.ts.map
--- src/repositories/url.repository.ts
+++ src/repositories/url.repository.ts
@@ -1,11 +1,20 @@
+import type { QueryResult } from "pg";
 import { query } from "../db/index.js";
 
-// save in db
-export const saveToDB = async (newRecord: {
+// new record type
+type newRecordType = {
   id: string;
   shortCode: string;
   longURL: string;
-}) => {
+};
+
+// short code type
+type shortCodeType = string;
+
+// save in db
+export const saveToDB = async (
+  newRecord: newRecordType,
+): Promise<QueryResult<any> | undefined> => {
   try {
     let queryText = `INSERT INTO urls (id, short_code, long_url) 
                     values ($1, $2, $3)
@@ -21,7 +30,9 @@ export const saveToDB = async (newRecord: {
 };
 
 // get from db
-export const getFromDB = async (shortCode: string) => {
+export const getFromDB = async (
+  shortCode: shortCodeType,
+): Promise<QueryResult<any> | undefined> => {
   try {
     let queryText = "SELECT long_url from urls where short_code = $1";
     let result = await query(queryText, [shortCode]);
