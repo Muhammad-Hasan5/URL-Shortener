5da29bfad0bf45a93c432689e52930e2b5e0b331
Muhammad Hasan <hasanamir8901@gmail.com>
2026-05-06 23:46:49 +0500

    refactor: replace some code lines

--- src/services/cache.service.ts
+++ src/services/cache.service.ts
@@ -1,21 +1,21 @@
 import redis from "../config/redis.js";
 
-const PREFIX = "url:"
+const PREFIX = "url:";
 
 export function setToCache(shortCode: string, longURL: string) {
-    try {
-        redis.set(PREFIX + shortCode, longURL, "EX", 60 * 60 * 24)
-    } catch (error: any) {
-        console.log("error saving to cache", error);
-    }
+  try {
+    redis.set(PREFIX + shortCode, longURL, "EX", 60 * 60 * 24);
+  } catch (error: any) {
+    console.log("error saving to cache", error);
+  }
 }
 
 export async function getFromCache(shortCode: string) {
-    try {
-        const res = await redis.get(PREFIX + shortCode)
-        return res
-    } catch (error: any) {
-        console.log("Error fetching from cache", error);
-        return null
-    }
+  try {
+    const res = await redis.get(PREFIX + shortCode);
+    return res;
+  } catch (error: any) {
+    console.log("Error fetching from cache", error);
+    return null;
+  }
 }
