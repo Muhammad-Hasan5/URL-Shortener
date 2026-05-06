70e2adbac6199b51af6a01cbaa81364758635b97
Muhammad Hasan <hasanamir8901@gmail.com>
2026-05-07 04:50:31 +0500

    refactor code

--- src/app.ts
+++ src/app.ts
@@ -3,13 +3,13 @@ import router from "./routes/url.route.js";
 
 const app: express.Application = express();
 
-app.use(express.json({limit: "5mb"}))
-app.use(express.urlencoded({limit: "5mb", extended: true}))
+app.use(express.json({ limit: "5mb" }));
+app.use(express.urlencoded({ limit: "5mb", extended: true }));
 
-app.use("/", router)
+app.use("/", router);
 
 app.get("/", (req, res) => {
-    res.send("hello world")
-})
+  res.send("hello world");
+});
 
 export default app;
