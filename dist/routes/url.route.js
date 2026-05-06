00c8a24ebf2f1b1a3542e06291e87ebccfef9beb
Muhammad Hasan <hasanamir8901@gmail.com>
2026-05-07 04:53:00 +0500

    add: rate limit middleware to route

--- dist/routes/url.route.js
+++ dist/routes/url.route.js
@@ -1,7 +1,8 @@
 import { Router } from "express";
 import { shortURL, redirect } from "../controllers/url.controller.js";
+import limiter from "../middlewares/rateLimit.js";
 const router = Router();
-router.route("/shorten").post(shortURL);
+router.route("/shorten").post(limiter, shortURL);
 router.route("/:shortCode").get(redirect);
 export default router;
 //# sourceMappingURL=url.route.js.map
--- src/routes/url.route.ts
+++ src/routes/url.route.ts
@@ -1,9 +1,10 @@
 import { Router } from "express";
 import { shortURL, redirect } from "../controllers/url.controller.js";
+import limiter from "../middlewares/rateLimit.js";
 
 const router = Router();
 
-router.route("/shorten").post(shortURL);
+router.route("/shorten").post(limiter, shortURL);
 router.route("/:shortCode").get(redirect);
 
 export default router;
