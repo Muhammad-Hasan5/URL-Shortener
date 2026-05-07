a257fb6ad50a9db40dafa5a3f7efb5afedf191f9
Muhammad Hasan <hasanamir8901@gmail.com>
2026-05-07 05:17:12 +0500

    add: graceful shutdown

--- dist/index.js
+++ dist/index.js
@@ -1,6 +1,15 @@
 process.loadEnvFile();
 import app from "./app.js";
-app.listen(process.env.PORT, () => {
+import { PgPool } from "./db/index.js";
+import redis from "./config/redis/index.redis.js";
+const server = app.listen(process.env.PORT, () => {
     console.log(`App is running on port http://localhost:${process.env.PORT}`);
+});
+process.on("SIGTERM", async () => {
+    server.close(async () => {
+        await PgPool.end();
+        await redis.quit();
+        process.exit(0);
+    });
 });
 //# sourceMappingURL=index.js.map
