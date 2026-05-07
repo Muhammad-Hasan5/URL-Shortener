4488213eab8a6146c477f2921de575a37dcd940b
Muhammad Hasan <hasanamir8901@gmail.com>
2026-05-07 06:23:58 +0500

    add: two endpoints, one for health check and other for system readiness

--- dist/app.js
+++ dist/app.js
@@ -1,11 +1,41 @@
 import express from "express";
 import router from "./routes/url.route.js";
+import { checkPoolReady } from "./db/index.js";
+import redis from "./config/redis/index.redis.js";
 const app = express();
 app.use(express.json({ limit: "5mb" }));
 app.use(express.urlencoded({ limit: "5mb", extended: true }));
 app.use("/", router);
-app.get("/", (req, res) => {
-    res.send("hello world");
+// health check end point
+app.get("/health/live", (req, res) => {
+    res.status(200).json({
+        message: "Server is healthy and live."
+    });
+});
+// server check ready-ness for traffic handling 
+app.get("/health/ready", async (req, res) => {
+    const dbStatus = await checkPoolReady();
+    if (dbStatus == true && redis.status === "ready") {
+        res.status(200).json({
+            status: "ready",
+            database: "up",
+            cache: "up",
+        });
+    }
+    else if (dbStatus == true && redis.status !== "ready") {
+        res.status(200).json({
+            status: "degraded",
+            database: "up",
+            cache: "down",
+        });
+    }
+    else {
+        res.status(503).json({
+            status: "not_ready",
+            database: "down",
+            cache: "up/down",
+        });
+    }
 });
 export default app;
 //# sourceMappingURL=app.js.map
--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,9 @@
 import express from "express";
 import router from "./routes/url.route.js";
 
+import { checkPoolReady } from "./db/index.js";
+import redis from "./config/redis/index.redis.js";
+
 const app: express.Application = express();
 
 app.use(express.json({ limit: "5mb" }));
@@ -8,8 +11,36 @@ app.use(express.urlencoded({ limit: "5mb", extended: true }));
 
 app.use("/", router);
 
-app.get("/", (req, res) => {
-  res.send("hello world");
+// health check end point
+app.get("/health/live", (req, res) => {
+  res.status(200).json({
+    message: "Server is healthy and live."
+  });
 });
+
+// server check ready-ness for traffic handling 
+app.get("/health/ready", async (req, res) => {
+  const dbStatus = await checkPoolReady()
+
+  if(dbStatus == true && redis.status === "ready"){
+    res.status(200).json({
+      status: "ready",
+      database: "up",
+      cache: "up",
+    })
+  } else if(dbStatus == true && redis.status !== "ready"){
+    res.status(200).json({
+      status: "degraded",
+      database: "up",
+      cache: "down",
+    });
+  } else {
+    res.status(503).json({
+      status: "not_ready",
+      database: "down",
+      cache: "up/down",
+    });
+  }
+})
 
 export default app;
