d4af145153a64787feaf88e7effc1bbb7465157c
Muhammad Hasan <hasanamir8901@gmail.com>
2026-05-09 03:19:45 +0500

    add: logging, fix: database table and indexing

---
+++ src/config/db/index.db.ts
@@ -0,0 +1,76 @@
+import { Pool } from "pg";
+
+import logger from "../pino-logging/index.pino.js";
+import env from "../env.js";
+
+export const PgPool = new Pool({
+  connectionString: String(env.data?.PG_CONNECTION_STRING),
+  max: 12,
+  connectionTimeoutMillis: 3000,
+  idleTimeoutMillis: 30000,
+  allowExitOnIdle: true,
+});
+
+logger.info("pg pool created successfully");
+
+//ready check method
+export const checkPoolReady = async (): Promise<boolean> => {
+  try {
+    await PgPool.query("SELECT 1");
+    logger.info("DB-pg pool ready");
+    return true;
+  } catch (error: any) {
+    logger.error("DB-pg pool not ready", error);
+    return false;
+  }
+};
+
+// DB queries:
+//general query function
+export const query = async (queryText: string, values?: any[]) => {
+  try {
+    return await PgPool.query(queryText, values);
+  } catch (error: any) {
+    logger.error("error fetching data", error.stack);
+  }
+};
+
+// table creation
+const createTable = async () => {
+  try {
+    const query = `
+      CREATE TABLE IF NOT EXISTS urls (
+        id BIGINT PRIMARY KEY,
+        short_code VARCHAR(10) UNIQUE NOT NULL,
+        long_url TEXT NOT NULL,
+        click_count INT DEFAULT 0,
+        expires_at TIMESTAMP NULL,
+        deleted_at TIMESTAMP NULL,
+        created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+      );
+    `;
+    await PgPool.query(query);
+    logger.info("Table created successfully");
+  } catch (error: any) {
+    logger.error("error creating table", error.stack);
+  }
+};
+
+const createIndex = async () => {
+  try {
+    // create index on short_code
+    const idx_long_url = `CREATE INDEX IF NOT EXISTS idx_long_url 
+                        ON urls (long_url)`;
+
+    const idx_active_shortCodes = `CREATE INDEX IF NOT EXISTS idx_active_short_codes ON urls (short_code) WHERE deleted_at IS NULL`;
+
+    await PgPool.query(idx_long_url);
+    await PgPool.query(idx_active_shortCodes);
+    logger.info("Indexes created successfully");
+  } catch (error: any) {
+    logger.error("error creating index", error.stack);
+  }
+};
+
+await createTable();
+await createIndex();
