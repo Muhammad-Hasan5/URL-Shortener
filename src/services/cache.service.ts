5ff815ea5968ad91517b7f00234aaee515678f2e
Muhammad Hasan <hasanamir8901@gmail.com>
2026-05-09 03:27:25 +0500

    add logging and circuit breaker to cache requests

--- src/services/cache.service.ts
+++ src/services/cache.service.ts
@@ -1,27 +1,25 @@
 import {
   setBreaker,
   getBreaker,
-} from "../config/opossum/cacheCircuitBreaker.js";
-import {
-  type cacheRecordType,
-  type shortCodeType,
-} from "../config/redis/cache.redis.js";
+} from "../config/opossum-circuit-Breaker/cacheCircuitBreaker.opossum.js";
+import logger from "../config/pino-logging/index.pino.js";
+import { type CacheRecordType } from "../@types/cache/index.types.js";
 
 
 // saving to cache
-export function setToCache(cacheRecord: cacheRecordType): void {
+export function setToCache(cacheRecord: CacheRecordType): void {
   setBreaker.fire(cacheRecord).catch((err: any) => {
-    console.log("Cache set failed:", err.message);
+    logger.error("Cache set failed:", err.message);
   });
 }
 
 
 // getting from cache
-export async function getFromCache(shortCode: shortCodeType): Promise<any> {
+export async function getFromCache(shortCode: string): Promise<any> {
   try {
     return await getBreaker.fire(shortCode);
   } catch (error: any) {
-    console.log("Error fetching from cache", error);
+    logger.error("Error fetching from cache", error);
     return null;
   }
 }
