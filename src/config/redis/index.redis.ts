53055a019ddbd70e47c27f122ec516fbe13ef243
Muhammad Hasan <hasanamir8901@gmail.com>
2026-05-09 03:23:44 +0500

    fix redis client configurations and add logging

--- src/config/redis/index.redis.ts
+++ src/config/redis/index.redis.ts
@@ -1,23 +1,46 @@
-process.loadEnvFile();
-
 import { Redis } from "ioredis";
+import logger from "../pino-logging/index.pino.js";
+import env from "../env.js";
 
 // creating redis client
 
 let redis: Redis;
 
-if(process.env.ENV === "DEVELOPMENT"){
-    redis = new Redis() //local
+if (env.data?.ENV === "DEVELOPMENT") {
+  redis = new Redis({
+    host: "localhost",
+    port: 6379,
+    lazyConnect: true,
+    enableOfflineQueue: false,
+    maxRetriesPerRequest: 1,
+    retryStrategy(times) {
+      if (times > 2) {
+        logger.info("Local Redis unavailable");
+        return null;
+      }
+      return 100;
+    },
+  }); //local
 } else {
-    redis = new Redis(process.env.REDIS_URL as string); //upstash => cloud
+  redis = new Redis(env.data?.REDIS_URL as string, {
+    lazyConnect: true,
+    enableOfflineQueue: false,
+    maxRetriesPerRequest: 1,
+    retryStrategy(times) {
+      if (times > 3) {
+        logger.info("Redis unavailable. Continuing without cache");
+        return null;
+      }
+      return Math.min(times * 200, 2000);
+    },
+  }); //upstash => cloud
 }
 
-redis.on("connect", () => console.log("REDIS CONNECTED"));
-redis.on("error", () => console.log("REDIS ERROR"));
+logger.info("redis client created successfully");
+
+redis.on("connect", () => logger.info("REDIS CONNECTED"));
+redis.on("error", () => logger.error("REDIS ERROR"));
+redis.on("close", () => logger.info("Redis connection closed"));
+redis.on("reconnecting", () => logger.info("Redis reconnecting..."));
 
 export default redis;
-
-
-
-
-
