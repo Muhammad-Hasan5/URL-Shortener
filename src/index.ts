ccb647ffe6850b8822126299f66d9c96c228a7f3
Muhammad Hasan <hasanamir8901@gmail.com>
2026-05-16 20:30:58 +0500

    run click count flush job at server starter

--- src/index.ts
+++ src/index.ts
@@ -3,9 +3,13 @@ import { PgPool } from "./db/pool.db.js";
 import redis from "./config/redis/index.redis.js";
 import logger from "./config/pino-logging/index.pino.js";
 import env from "./config/env.js";
+import { startFlushingClicks } from "./jobs/clickFlush.job.js";
 
-const server = app.listen(env.data?.PORT, () => {
+const server = app.listen(env.data?.PORT, async () => {
   logger.info(`App is running on port ${env.data?.BASE_URL}:${env.data?.PORT}`);
+  
+  // running flush for click count form redis to PG
+  await startFlushingClicks();
 });
 
 process.on("SIGTERM", async (): Promise<any> => {
