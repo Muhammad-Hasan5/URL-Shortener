43015dd1ecc0e0119103cc7afee46298e5976459
Muhammad Hasan <hasanamir8901@gmail.com>
2026-05-16 21:25:24 +0500

    update cache record type

--- src/@types/cache/index.types.ts
+++ src/@types/cache/index.types.ts
@@ -3,6 +3,7 @@ import z from "zod";
 const cacheRecordSchema = z.object({
   shortCode: z.string(),
   longURL: z.string(),
+  EXPIRY: z.number()
 });
 
 export type CacheRecordType = z.infer<typeof cacheRecordSchema>;
