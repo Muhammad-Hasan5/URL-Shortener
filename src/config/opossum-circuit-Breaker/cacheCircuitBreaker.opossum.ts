665fcf7aa4c4962485802de3a5f9748694dcbe74
Muhammad Hasan <hasanamir8901@gmail.com>
2026-05-19 00:24:55 +0500

    add click count increment, get TTL and update TTL breaker for redis

--- src/config/opossum-circuit-Breaker/cacheCircuitBreaker.opossum.ts
+++ src/config/opossum-circuit-Breaker/cacheCircuitBreaker.opossum.ts
@@ -1,7 +1,15 @@
 import createBreaker from "./index.opossum.js";
-import { set, get, incr } from "../redis/cache.redis.js";
+import {
+  set,
+  get,
+  incr,
+  getKeyTTL,
+  updateKeyTTL,
+} from "../redis/cache.redis.js";
 
 // creating breakers
 export const setBreaker = createBreaker(set);
 export const getBreaker = createBreaker(get);
-export const incBreaker = createBreaker(incr)
+export const incBreaker = createBreaker(incr);
+export const getKeyTTLBreaker = createBreaker(getKeyTTL);
+export const updateKeyTTLBreaker = createBreaker(updateKeyTTL);
