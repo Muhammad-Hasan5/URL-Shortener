592e4748226fbd6da7a84e20e6cec2958b3077b7
Muhammad Hasan <hasanamir8901@gmail.com>
2026-05-19 00:26:16 +0500

    refactor some code in table creation migration

--- src/db/migrations/20260515195306_create_urls_table.ts
+++ src/db/migrations/20260515195306_create_urls_table.ts
@@ -8,7 +8,7 @@ export async function up(knex: Knex): Promise<void> {
     table.string("user_id").nullable();
     table.integer("click_count").defaultTo(0).notNullable();
     table.timestamp("created_at", { useTz: true }).defaultTo(knex.fn.now());
-    table.timestamp("last_accessed_at", {useTz: true}).nullable()
+    table.timestamp("last_accessed_at", {useTz: true}).nullable();
     table.timestamp("expires_at", { useTz: true }).nullable();
     table.timestamp("deleted_at", { useTz: true }).nullable();
 
