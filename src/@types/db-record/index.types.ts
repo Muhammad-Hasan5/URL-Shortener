6feb9b951ce27b0c78a5a2daaa2316cd4f2d7a75
Muhammad Hasan <hasanamir8901@gmail.com>
2026-05-19 00:23:50 +0500

    update new db record schema and type

--- src/@types/db-record/index.types.ts
+++ src/@types/db-record/index.types.ts
@@ -1,9 +1,13 @@
 import z from "zod"
 
 const newRecordSchema = z.object({
-    id: z.string(),
-    shortCode: z.string(),
-    longURL: z.string()
-})
+  id: z.string(),
+  shortCode: z.string(),
+  longURL: z.string(),
+  click_count: z.int(),
+  expires_at: z.coerce.date(),
+  last_accessed_at: z.coerce.date(),
+  deleted_at: z.coerce.date()
+});
 
 export type NewRecordType = z.infer<typeof newRecordSchema>
