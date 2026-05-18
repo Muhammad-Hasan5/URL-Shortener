101b72c15e9220e258b2a6b48cb92a38a1ce4e6d
Muhammad Hasan <hasanamir8901@gmail.com>
2026-05-19 01:43:47 +0500

    refactor extra logging

--- src/controllers/url.controller.ts
+++ src/controllers/url.controller.ts
@@ -1,5 +1,5 @@
 import type { Request, Response } from "express";
-import { generateShortCode } from "../services/shortCode.service.js";
+import { generateShortCode } from "../utils/shortCode.utils.js";
 import { saveToDB, getFromDB } from "../repositories/url.repository.js";
 import { query } from "../db/queries.db.js";
 import {
@@ -7,46 +7,21 @@ import {
   getFromCache,
   incClickCount,
   updateTTL,
-  ttl
-} from "../services/cache.service.js";
+  ttl,
+} from "../repositories/cache.repository.js";
 import logger from "../config/pino-logging/index.pino.js";
-import {
-  cacheRequests,
-  requestDuration,
-} from "../config/prometheus-metrics/index.prometheus.js";
-import { getTTL, shouldRefresh } from "../utils/cacheTTL.js";
+import { cacheRequests } from "../config/prometheus-metrics/index.prometheus.js";
+import { getTTL, shouldRefresh } from "../utils/cacheTTL.utils.js";
 import env from "../config/env.js";
 
 // covnert long url to SHORT one
 export const shortURL = async (req: Request, res: Response) => {
-  //start request timer metric
-  const end = requestDuration.startTimer({
-    method: "POST",
-    route: "url-shortener-route",
-  });
-
   const { longURL, expires_at } = req.body;
 
   //validate long url
   try {
     new URL(longURL);
   } catch {
-    //request end-time
-    const endtime = end({ status: "failed-400" });
-
-    //logging
-    logger.error(
-      {
-        id: req.id,
-        success: false,
-        statusCode: 400,
-        msg: "invalid longURL",
-        user_longURL: longURL,
-        latencyMS: Date.now() - endtime,
-      },
-      "short-a-url.unsuccessful",
-    );
-
     return res.status(400).json({
       status: 400,
       success: false,
@@ -59,17 +34,8 @@ export const shortURL = async (req: Request, res: Response) => {
 
   while (attempts < 3) {
     try {
-      logger.info({ checkPoint_code_generator: "starting to generate code" });
-
       // call service to short url
       const result = generateShortCode();
-
-      logger.info({
-        checkPoint_code_generator: "short code generated",
-        shortcode: result,
-      });
-
-      logger.info({ checkPoint_cache: "starting getting from cache" });
 
       // checking if already in cache or not, escaping duplication
       const cacheResult = await getFromCache(result.shortCode);
@@ -100,23 +66,6 @@ export const shortURL = async (req: Request, res: Response) => {
         logger.info({ chechPoint_DB: "fetched results from DB" });
 
         if ((existing?.rows.length as number) > 0) {
-          //request end-time
-          const endtime = end({ status: "success-200" });
-
-          //logging
-          logger.info(
-            {
-              id: req.id,
-              success: true,
-              statusCode: 200,
-              long_url: longURL,
-              shortCode: existing?.rows[0].short_code,
-              msg: "longURL already shorted previously",
-              latencyMS: Date.now() - endtime,
-            },
-            "short-a-url.already_shorten.successful",
-          );
-
           return res.status(200).json({
             success: true,
             shortURL: `${env.data?.BASE_URL}:${env.data?.PORT}/${existing?.rows[0].short_code}`,
@@ -139,11 +88,14 @@ export const shortURL = async (req: Request, res: Response) => {
         );
 
         //refreshing ttl if near to expire
-        const remainingTtl = await ttl(cacheResult.shortCode)
-        
-        if(shouldRefresh(Number(remainingTtl), cacheResult.cachedTtl)){
-          const freshTtl = getTTL(cacheResult.clickCount, new Date(cacheResult.createdAt))
-          await updateTTL(cacheResult.shortCode, freshTtl)
+        const remainingTtl = await ttl(cacheResult.shortCode);
+
+        if (shouldRefresh(Number(remainingTtl), cacheResult.cachedTtl)) {
+          const freshTtl = getTTL(
+            cacheResult.clickCount,
+            new Date(cacheResult.createdAt),
+          );
+          await updateTTL(cacheResult.shortCode, freshTtl);
         }
 
         return res.status(200).json({
@@ -151,8 +103,6 @@ export const shortURL = async (req: Request, res: Response) => {
           data: cacheResult,
         });
       }
-
-      logger.info({ checkPoint_DB: "starting saving to database" });
 
       //TODO add auth
       //const date = new Date()
@@ -166,10 +116,6 @@ export const shortURL = async (req: Request, res: Response) => {
         expires_at: new Date(expires_at),
       });
 
-      logger.info({ checkPoint_DB: "saved to database" });
-
-      logger.info({ checkPoint_cache: "starting saving to cache" });
-
       const TTL = getTTL(0, new Date(Date.now()));
 
       //save new record to cache
@@ -182,27 +128,8 @@ export const shortURL = async (req: Request, res: Response) => {
         cachedTtl: TTL,
       });
 
-      logger.info({ checkPoint_cache: "saved to cache (maybe)" });
-
       //response
       const baseURL = `${env.data?.BASE_URL}:${env.data?.PORT}`;
-
-      //request end-time
-      const endtime = end({ status: "success-201" });
-
-      //logging
-      logger.info(
-        {
-          id: req.id,
-          success: true,
-          statusCode: 201,
-          long_url: longURL,
-          shortCode: result.shortCode,
-          mag: "longURL shorten successfully",
-          latencyMS: Date.now() - endtime,
-        },
-        "short-a-url.shorten-successful",
-      );
 
       return res.status(201).json({
         status: 201,
@@ -220,61 +147,22 @@ export const shortURL = async (req: Request, res: Response) => {
         attempts++;
         continue;
       }
-
-      //request end-time
-      const endtime = end({ status: "failed-400" });
-
-      //logging
-      logger.error(
-        {
-          id: req.id,
-          success: false,
-          statusCode: 400,
-          long_url: longURL,
-          mag: "request failed",
-          latencyMS: Date.now() - endtime,
-        },
-        "short-a-url.shorten-unsuccessful.request-failed",
-      );
     }
   }
 };
 
 // REDIRECT to long url
 export const redirect = async (req: Request, res: Response) => {
-  // starting request time
-  const end = requestDuration.startTimer({
-    method: "REDIRECT",
-    route: "redirect-to-longURL-route",
-  });
-
   const shortCode = req.params.shortCode;
 
   //validate param: short code
   if (!shortCode) {
-    //request end-time
-    const endtime = end({ status: "failed-400" });
-
-    //logging
-    logger.error(
-      {
-        id: req.id,
-        success: false,
-        statusCode: 400,
-        msg: "invalid short code",
-        latencyMS: Date.now() - endtime,
-      },
-      "redirect-to-longURL.unsuccessful",
-    );
-
     return res.status(400).json({
       status: 400,
       success: false,
       msg: "short code is not available",
     });
   }
-
-  logger.info({ checkPoint_cache: "starting getting from cache" });
 
   // check cache to get redirect long url
   const cacheResult = await getFromCache(`url:${String(shortCode)}`);
@@ -294,39 +182,17 @@ export const redirect = async (req: Request, res: Response) => {
       "redirect-to-longURL.cache_missed",
     );
 
-    logger.info({ checkPoint_DB: "starting fetching long url from DB" });
-
     // call db to get redirect long url
     const result = await getFromDB(shortCode as string);
 
-    logger.info({ checkPoint_DB: "fetched results from DB" });
-
     //validate DB results
     if (result?.rows.length === 0) {
-      //request end-time
-      const endtime = end({ status: "failed-404" });
-
-      //logging
-      logger.error(
-        {
-          id: req.id,
-          success: false,
-          statusCode: 404,
-          db_result: result,
-          msg: "url not exists in DB",
-          latencyMS: Date.now() - endtime,
-        },
-        "redirect-to-longURL.unsuccessful",
-      );
-
       return res.status(404).json({
         status: 404,
         success: false,
         msg: "url not exist in DB",
       });
     }
-
-    logger.info({ checkPoint_cache: "starting to save to cache" });
 
     const clickCount = result?.rows[0].click_count;
     const created_at = result?.rows[0].created_at;
@@ -341,25 +207,6 @@ export const redirect = async (req: Request, res: Response) => {
       expiresAt: result?.rows[0].expires_at,
       cachedTtl: TTL,
     });
-
-    logger.info({
-      checkPoint_cache: "maybe saved to cache as it is a silent operation",
-    });
-
-    //request end-time
-    const endtime = end({ status: "successful-302" });
-
-    //logging
-    logger.info(
-      {
-        id: req.id,
-        success: true,
-        statusCode: 302,
-        msg: "redirect successful",
-        latencyMS: Date.now() - endtime,
-      },
-      "redirect-to-longURL.successful",
-    );
 
     // redirect
     return res.redirect(302, result!.rows[0].long_url);
