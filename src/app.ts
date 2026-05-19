04028d11682db4b7d240653fbbea44911de167c1
Muhammad Hasan <hasanamir8901@gmail.com>
2026-05-19 22:54:26 +0500

    mount healthcheck routes to express app

--- src/app.ts
+++ src/app.ts
@@ -1,13 +1,7 @@
 import express from "express";
-import router from "./routes/url.route.js";
+import urlRouter from "./routes/url.route.js";
+import healthCheckRouter from "./routes/healthcheck.route.js";
 import { logRequestID } from "./middlewares/logRequestID.middleware.js";
-
-import { checkPoolReady } from "./db/pool.db.js";
-import redis from "./config/redis/index.redis.js";
-
-import register from "./config/prometheus-metrics/index.prometheus.js";
-
-import logger from "./config/pino-logging/index.pino.js";
 import { requestLogger } from "./middlewares/requestLogger.middleware.js";
 
 const app: express.Application = express();
@@ -18,67 +12,11 @@ app.use(express.urlencoded({ limit: "5mb", extended: true }));
 //middleware to attach requestid for each
 // log to Global Request of express
 app.use(logRequestID);
-app.use(requestLogger)
+app.use(requestLogger);
 
-//attaching router to app
-app.use("/", router);
-
-
-//TODO create a separate controllers & route for health checkpoints
-// health check end point
-app.get("/health/live", (req, res) => {
-  res.status(200).json({
-    message: "Server is healthy and live.",
-  });
-});
-
-// server check ready-ness for traffic handling
-app.get("/health/ready", async (req, res) => {
-  const dbStatus = await checkPoolReady();
-
-  if (dbStatus == true && redis.status === "ready") {
-    logger.info({
-      status: "ready",
-      database: "up",
-      cache: "up",
-    });
-
-    res.status(200).json({
-      status: "ready",
-      database: "up",
-      cache: "up",
-    });
-  } else if (dbStatus == true && redis.status !== "ready") {
-    logger.info({
-      status: "degraded",
-      database: "up",
-      cache: "down",
-    });
-
-    res.status(200).json({
-      status: "degraded",
-      database: "up",
-      cache: "down",
-    });
-  } else {
-    logger.info({
-      status: "not_ready",
-      database: "down",
-      cache: "up/down",
-    });
-
-    res.status(503).json({
-      status: "not_ready",
-      database: "down",
-      cache: "up/down",
-    });
-  }
-});
-
-// metrics endpoint
-app.get("/metrics", async (req, res) => {
-  res.set("Content-Type", register.contentType);
-  res.send(await register.metrics());
-});
+//attaching url-router to app
+app.use("/", urlRouter);
+//attaching healthcheck-router to app
+app.use("/", healthCheckRouter);
 
 export default app;
