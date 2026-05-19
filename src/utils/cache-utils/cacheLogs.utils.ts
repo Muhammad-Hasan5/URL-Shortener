228e0784dbc00078edc87b3ae477d5dc4ed06c67
Muhammad Hasan <hasanamir8901@gmail.com>
2026-05-19 22:57:18 +0500

    fix: some import paths

--- src/utils/cache-utils/cacheLogs.utils.ts
+++ src/utils/cache-utils/cacheLogs.utils.ts
@@ -1,5 +1,5 @@
-import { cacheRequests } from "../config/prometheus-metrics/index.prometheus.js";
-import logger from "../config/pino-logging/index.pino.js";
+import { cacheRequests } from "../../config/prometheus-metrics/index.prometheus.js";
+import logger from "../../config/pino-logging/index.pino.js";
 
 interface ILogObject {
   reqId: any;
