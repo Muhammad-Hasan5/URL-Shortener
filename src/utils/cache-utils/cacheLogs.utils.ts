514f513b49dfbc67ca91c791b791a5e245e397e3
Muhammad Hasan <hasanamir8901@gmail.com>
2026-05-19 22:12:13 +0500

    add a cache logging hit and miss methods separately for metrics calculations and hit-miss logging

---
+++ src/utils/cache-utils/cacheLogs.utils.ts
@@ -0,0 +1,43 @@
+import { cacheRequests } from "../config/prometheus-metrics/index.prometheus.js";
+import logger from "../config/pino-logging/index.pino.js";
+
+interface ILogObject {
+  reqId: any;
+  reqMethod: string;
+  cacheMethod: string;
+  cacheResult?: any;
+  route: string;
+}
+
+export function cacheHitLog(logObject: ILogObject): void {
+  /* cache hit-miss counter */
+  cacheRequests.inc({ result: "hit", cache_method: logObject.cacheMethod });
+
+  //logging cache-hit
+  logger.info(
+    {
+      id: logObject.reqId,
+      request_method: logObject.reqMethod,
+      cache_method: logObject.cacheMethod,
+      cache_result: logObject.cacheResult,
+      route: logObject.route,
+    },
+    `${logObject.route}.cache_hit`,
+  );
+}
+
+export function cacheMissLog(logObject: ILogObject): void {
+  /* cache hit-miss counter */
+  cacheRequests.inc({ result: "miss", cache_method: logObject.cacheMethod });
+
+  //logging cache-miss
+  logger.info(
+    {
+      id: logObject.reqId,
+      request_method: logObject.reqMethod,
+      cache_method: logObject.cacheMethod,
+      route: logObject.route,
+    },
+    `${logObject.route}.cache_missed`,
+  );
+}
