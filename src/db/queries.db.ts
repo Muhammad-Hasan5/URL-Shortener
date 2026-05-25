c0a9428c260ca57fa5a07cd81c819268bbd7c5bf
Muhammad Hasan <hasanamir8901@gmail.com>
2026-05-25 05:23:55 +0500

    fix: update query function to specify return type and improve error handling

--- src/db/queries.db.ts
+++ src/db/queries.db.ts
@@ -1,12 +1,17 @@
 import { PgPool } from "./pool.db.js";
 import logger from "../config/pino-logging/index.pino.js";
+import type { QueryResult } from "pg";
 
 // DB queries:
 //general query function
-export const query = async (queryText: string, values?: any[]) => {
+export const query = async (
+  queryText: string,
+  values?: any[],
+): Promise<QueryResult<any> | null> => {
   try {
     return await PgPool.query(queryText, values);
   } catch (error: any) {
     logger.error("error fetching data", error.stack);
+    return null;
   }
 };
