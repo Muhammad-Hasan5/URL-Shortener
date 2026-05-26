32028f6134f7d98cb5ca9774766f5a2c91b004fe
Muhammad Hasan <hasanamir8901@gmail.com>
2026-05-27 04:44:53 +0500

    fix: remove logging middlewares from global server instance, attached them to specific routes

--- src/app.ts
+++ src/app.ts
@@ -1,22 +1,17 @@
 import express from "express";
 import urlRouter from "./routes/url.route.js";
 import healthCheckRouter from "./routes/healthcheck.route.js";
-import { logRequestID } from "./middlewares/logRequestID.middleware.js";
-import { requestLogger } from "./middlewares/requestLogger.middleware.js";
+
 
 const app: express.Application = express();
 
 app.use(express.json({ limit: "5mb" }));
 app.use(express.urlencoded({ limit: "5mb", extended: true }));
 
-//middleware to attach requestid for each
-// log to Global Request of express
-app.use(logRequestID);
-app.use(requestLogger);
-
+//attaching healthcheck-router to app
+app.use("/api", healthCheckRouter);
 //attaching url-router to app
 app.use("/", urlRouter);
-//attaching healthcheck-router to app
-app.use("/", healthCheckRouter);
+
 
 export default app;
