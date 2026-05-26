16f2f1f00203eb6858c4f41120e57b316c59811a
Muhammad Hasan <hasanamir8901@gmail.com>
2026-05-27 04:45:51 +0500

    add: add logging middlewares to routes accessible by users

--- src/routes/url.route.ts
+++ src/routes/url.route.ts
@@ -1,10 +1,12 @@
 import { Router } from "express";
 import { shortURL, redirect } from "../controllers/url.controller.js";
 import limiter from "../middlewares/rateLimit.middleware.js";
+import { requestLogger } from "../middlewares/requestLogger.middleware.js";
+import { logRequestID } from "../middlewares/logRequestID.middleware.js";
 
 const router = Router();
 
-router.route("/shorten").post(limiter, shortURL);
-router.route("/:shortCode").get(redirect);
+router.route("/shorten").post(limiter, logRequestID, requestLogger, shortURL);
+router.route("/:shortCode").get(logRequestID, requestLogger, redirect);
 
 export default router;
