fdaa717c6f4641c5c98acb496d230f35a2138bfd
Muhammad Hasan <hasanamir8901@gmail.com>
2026-05-27 03:45:48 +0500

    fix: set click_count to default 0, expires_at and last_accessed_at to be nullable in new database record

--- src/@types/db-record/index.types.ts
+++ src/@types/db-record/index.types.ts
@@ -4,12 +4,12 @@ const newRecordSchema = z.object({
   id: z.string(),
   shortCode: z.string(),
   longURL: z.string(),
-  click_count: z.int(),
-  user_id: z.string(),
-  created_at: z.coerce.date(),
-  expires_at: z.coerce.date(),
-  last_accessed_at: z.coerce.date(),
-  deleted_at: z.coerce.date()
+  click_count: z.number().int().default(0),
+  user_id: z.string().nullable().optional(),
+  created_at: z.coerce.date().optional(),
+  expires_at: z.coerce.date().nullable().optional(),
+  last_accessed_at: z.coerce.date().nullable().optional(),
+  deleted_at: z.coerce.date().nullable().optional(),
 });
 
 export type NewRecordType = z.infer<typeof newRecordSchema>
