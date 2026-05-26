aa3824b3fdf52692e905e6c855ea557eab943211
Muhammad Hasan <hasanamir8901@gmail.com>
2026-05-27 04:45:22 +0500

    refactor: change metrics parameters and calculations

--- src/config/prometheus-metrics/index.prometheus.ts
+++ src/config/prometheus-metrics/index.prometheus.ts
@@ -1,29 +1,25 @@
 import client from "prom-client";
 
-const Registery = client.Registry;
-const register = new Registery();
+const Registry = client.Registry;
+const register = new Registry();
 
 client.collectDefaultMetrics({ register });
 
-// a histogram for request duration
-// metrics checking a req completion time
 export const requestDuration = new client.Histogram({
-  name: "http_request_duration_ms",
-  help: "Duration of HTTP requests in ms",
+  name: "http_request_duration_seconds",
+  help: "Duration of HTTP requests in seconds",
   labelNames: ["method", "route", "status"],
-  buckets: [5, 10, 25, 50, 100, 250, 500],
+  buckets: [0.005, 0.01, 0.025, 0.05, 0.1, 0.25, 0.5],
 });
 
-register.registerMetric(requestDuration)
+register.registerMetric(requestDuration);
 
-//calculating number of cache requests 
-// segmented on hit or miss
 export const cacheRequests = new client.Counter({
-  name: "cache_requets",
-  help: "Total number of cache requests, labeled by result (hit/miss).",
+  name: "cache_requests",
+  help: "Total cache requests segmented by hit/miss",
   labelNames: ["result", "cache_method"],
 });
 
 register.registerMetric(cacheRequests);
 
-export default register
+export default register;
