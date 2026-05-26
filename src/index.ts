c461928fac6cdd256ed0f46e6986eae275da25b2
Muhammad Hasan <hasanamir8901@gmail.com>
2026-05-26 23:58:58 +0500

    fix: refactor server startup to include Redis connection handling and error logging

--- src/index.ts
+++ src/index.ts
@@ -5,19 +5,31 @@ import logger from "./config/pino-logging/index.pino.js";
 import env from "./config/env.js";
 import { startFlushingClicks } from "./jobs/clickFlush.job.js";
 
-const server = app.listen(env.data?.PORT, async () => {
-  logger.info(`App is running on port ${env.data?.BASE_URL}:${env.data?.PORT}`);
-  
-  // running flush for click count form redis to PG
-  await startFlushingClicks();
-});
+async function start() {
+  try {
+    await redis.connect()
+  } catch (err: any) {
+    logger.warn({ err }, "redis.startup.failed — running without cache");
+  }
 
-process.on("SIGTERM", async (): Promise<any> => {
-  logger.info("closing system");
+  const server = app.listen(env.data?.PORT, async () => {
+    logger.info(
+      `App is running on port ${env.data?.BASE_URL}:${env.data?.PORT}`,
+    );
 
-  server.close(async () => {
-    await PgPool.end();
-    await redis.quit();
-    process.exit(0);
+    // running flush for click count form redis to PG
+    startFlushingClicks();
   });
-});
+
+  process.on("SIGTERM", async (): Promise<any> => {
+    logger.info("closing system");
+
+    server.close(async () => {
+      await PgPool.end();
+      await redis.quit();
+      process.exit(0);
+    });
+  });
+}
+
+start()
