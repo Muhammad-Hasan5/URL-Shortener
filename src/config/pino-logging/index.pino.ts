1c5961cc320cb332e48fee9cd873d6d0ad978cb2
Muhammad Hasan <hasanamir8901@gmail.com>
2026-05-27 01:37:05 +0500

    fix: update logger configuration to directly use LOG_LEVEL from env

--- src/config/pino-logging/index.pino.ts
+++ src/config/pino-logging/index.pino.ts
@@ -2,7 +2,7 @@ import pino from "pino";
 import env from "../env.js";
 
 const logger = pino({
-  level: env.data?.LOG_LEVEL ?? "info"
+  level: env.LOG_LEVEL ?? "info"
 });
 
 export default logger;
