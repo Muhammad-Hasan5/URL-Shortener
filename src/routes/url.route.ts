bb7ab08b0b99582209c2cc026e06f4e0a8ead0fc
Muhammad Hasan <hasanamir8901@gmail.com>
2026-05-27 05:04:01 +0500

    fix: make the redirect route parameter dynamix restricting the mix up with health routes

--- src/routes/url.route.ts
+++ src/routes/url.route.ts
@@ -7,6 +7,8 @@ import { logRequestID } from "../middlewares/logRequestID.middleware.js";
 const router = Router();
 
 router.route("/shorten").post(limiter, logRequestID, requestLogger, shortURL);
-router.route("/:shortCode").get(logRequestID, requestLogger, redirect);
+router
+  .route("/:shortCode([A-Za-z0-9]{6,12})")
+  .get(logRequestID, requestLogger, redirect);
 
 export default router;
