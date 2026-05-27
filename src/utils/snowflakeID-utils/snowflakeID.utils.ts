64417526893c65bee4b300061a1122872e598376
Muhammad Hasan <hasanamir8901@gmail.com>
2026-05-28 03:40:26 +0500

    fix: change single method of snowflake id generator into class for acquiring machine id at the start of instance

---
+++ src/utils/snowflakeID-utils/snowflakeID.utils.ts
@@ -0,0 +1,57 @@
+import logger from "../../config/pino-logging/index.pino.js";
+
+export class SnowflakeGenerator {
+  /* date: November 14, 2023 ids will generate till 2092*/
+  private readonly EPOCH = 1700000000000;
+
+  private lastTimestamp = 0n;
+
+  private readonly machineID: bigint;
+
+  private sequence = 0n;
+
+  constructor(id: bigint) {
+    this.machineID = id;
+  }
+
+  generate(): bigint {
+    let now = BigInt(Date.now());
+
+    //check for drift in time
+    if (now < this.lastTimestamp) {
+      const drift = this.lastTimestamp - now;
+      if (drift > 100n) {
+        throw new Error(
+          `Clock drifted back ${drift}ms, refusing to generate ID`,
+        );
+      }
+      while (BigInt(Date.now()) <= this.lastTimestamp) {}
+      now = BigInt(Date.now());
+    }
+
+    //checking for same ms sequence allotment
+    if (now === this.lastTimestamp) {
+      this.sequence = (this.sequence + 1n) & 0xfffn;
+      if (this.sequence === 0n) {
+        while (BigInt(Date.now()) <= this.lastTimestamp) {}
+        now = BigInt(Date.now());
+      }
+    } else {
+      this.sequence = 0n;
+    }
+
+    this.lastTimestamp = now;
+
+    const snowflakeId =
+      ((now - BigInt(this.EPOCH)) << 22n) |
+      (this.machineID << 12n) |
+      this.sequence;
+
+    logger.debug(
+      { snowflakeId: snowflakeId.toString() },
+      "snowflake id generated",
+    );
+
+    return snowflakeId;
+  }
+}
