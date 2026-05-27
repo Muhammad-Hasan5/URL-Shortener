ac00e855c5161fc735783a69ac699aa970fea54b
Muhammad Hasan <hasanamir8901@gmail.com>
2026-05-27 06:22:16 +0500

    refactor: remove verbosive types

--- src/app.ts
+++ src/app.ts
@@ -3,7 +3,7 @@ import urlRouter from "./routes/url.route.js";
 import healthCheckRouter from "./routes/healthcheck.route.js";
 
 
-const app: express.Application = express();
+const app = express();
 
 app.use(express.json({ limit: "5mb" }));
 app.use(express.urlencoded({ limit: "5mb", extended: true }));
