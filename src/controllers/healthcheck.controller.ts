955301876645e284e781238ba3a650f6d89820df
Muhammad Hasan <hasanamir8901@gmail.com>
2026-06-06 01:59:51 +0500

    fix: correct the pool import

--- src/controllers/healthcheck.controller.ts
+++ src/controllers/healthcheck.controller.ts
@@ -1,9 +1,8 @@
 import type { Request, Response } from "express";
 import logger from "../config/pino-logging/index.pino.js";
 import redis from "../config/redis/index.redis.js";
-import { checkPoolReady } from "../db/pool.db.js";
+import { checkPoolReady } from "../db/pools.db.js";
 import register from "../config/prometheus-metrics/index.prometheus.js";
-
 
 export async function serverHealthCheck(req: Request, res: Response) {
   return res.status(200).json({
@@ -11,50 +10,49 @@ export async function serverHealthCheck(req: Request, res: Response) {
   });
 }
 
-export async function serverReadyCheck(req: Request, res: Response){
-    const dbStatus = await checkPoolReady();
+export async function serverReadyCheck(req: Request, res: Response) {
+  const dbStatus = await checkPoolReady();
 
-    if (dbStatus == true && redis.status === "ready") {
-      logger.info({
-        status: "ready",
-        database: "up",
-        cache: "up",
-      });
+  if (dbStatus == true && redis.status === "ready") {
+    logger.info({
+      status: "ready",
+      database: "up",
+      cache: "up",
+    });
 
-      return res.status(200).json({
-        status: "ready",
-        database: "up",
-        cache: "up",
-      });
-    } else if (dbStatus == true && redis.status !== "ready") {
-      logger.info({
-        status: "degraded",
-        database: "up",
-        cache: "down",
-      });
+    return res.status(200).json({
+      status: "ready",
+      database: "up",
+      cache: "up",
+    });
+  } else if (dbStatus == true && redis.status !== "ready") {
+    logger.info({
+      status: "degraded",
+      database: "up",
+      cache: "down",
+    });
 
-      return res.status(200).json({
-        status: "degraded",
-        database: "up",
-        cache: "down",
-      });
-    } else {
-      logger.info({
-        status: "not_ready",
-        database: "down",
-        cache: "up/down",
-      });
+    return res.status(200).json({
+      status: "degraded",
+      database: "up",
+      cache: "down",
+    });
+  } else {
+    logger.info({
+      status: "not_ready",
+      database: "down",
+      cache: "up/down",
+    });
 
-      return res.status(503).json({
-        status: "not_ready",
-        database: "down",
-        cache: "up/down",
-      });
-    }
-};
+    return res.status(503).json({
+      status: "not_ready",
+      database: "down",
+      cache: "up/down",
+    });
+  }
+}
 
-export async function apiMetrics(req: Request, res: Response){
-    res.set("Content-Type", register.contentType);
-    return res.send(await register.metrics());
-};
-
+export async function apiMetrics(req: Request, res: Response) {
+  res.set("Content-Type", register.contentType);
+  return res.send(await register.metrics());
+}
