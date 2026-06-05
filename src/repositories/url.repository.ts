1d87bc9a6ccc97e37607448690c18d554943ed84
Muhammad Hasan <hasanamir8901@gmail.com>
2026-06-06 02:01:58 +0500

    fix: correct the resolveLongURL service function flow, fix some bugs and imports

--- src/repositories/url.repository.ts
+++ src/repositories/url.repository.ts
@@ -1,19 +1,20 @@
 import type { QueryResult } from "pg";
-import { query } from "../db/queries.db.js";
 import logger from "../config/pino-logging/index.pino.js";
 import { type DatabaseRecord } from "../@types/db-record/index.types.js";
+import { getPool } from "../db/pools.db.js";
 
 // save in db
-export const saveToDB = async (
+export const createUrl = async (
   newRecord: Partial<DatabaseRecord>,
 ): Promise<QueryResult<any> | null> => {
   try {
+    const db = getPool("write")
     let queryText = `INSERT INTO urls (id, short_code, long_url) 
                     values ($1, $2, $3)
                     `;
 
     // querying DB
-    return await query(queryText, [
+    return await db.query(queryText, [
       newRecord.id,
       newRecord.shortCode,
       newRecord.longURL,
@@ -25,15 +26,30 @@ export const saveToDB = async (
 };
 
 // get from db
-export const getFromDB = async (
+export const findByShortCode = async (
   shortCode: string,
 ): Promise<QueryResult<any> | null> => {
   try {
+    const db = getPool("read")
     let queryText = "SELECT long_url from urls where short_code = $1";
-    let result = await query(queryText, [shortCode]);
+    let result = await db.query(queryText, [shortCode]);
     return result;
   } catch (error: any) {
     logger.error("error fetching url from DB", error.stack);
     return null
+  }
+};
+
+export const checkIfAlreadyExists = async (
+  longURL: string,
+): Promise<QueryResult<any> | null> => {
+  try {
+    const db = getPool("read");
+    let queryText = "SELECT * from urls where longURL = $1";
+    let result = await db.query(queryText, [longURL]);
+    return result;
+  } catch (error: any) {
+    logger.error("error fetching url from DB", error.stack);
+    return null;
   }
 };
