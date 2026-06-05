ddd5fae4cb9fbcce755b6f25c638b57c6766cc29
Muhammad Hasan <hasanamir8901@gmail.com>
2026-06-06 01:54:26 +0500

    add a shell script to create db readreplica on starting the container

---
+++ postgres/primary/init-primary.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+set -e
+
+psql -v ON_ERROR_STOP=1 -U "$POSTGRES_USER" <<-SQL
+    DO
+    \$do\$
+    BEGIN
+        IF NOT EXISTS (
+            SELECT FROM pg_catalog.pg_roles
+            WHERE rolname = 'replicator'
+        ) THEN
+            CREATE ROLE replicator 
+            WITH REPLICATION LOGIN PASSWORD '${REPLICA_PASS}';
+        END IF;
+    END
+    \$do\$;
+SQL
+
+grep -qxF 'wal_level = replica' "$PGDATA/postgresql.conf" || cat >> "$PGDATA/postgresql.conf" <<-EOF
+
+wal_level = replica
+max_wal_senders = 3
+wal_keep_size = 256MB
+hot_standby = on
+EOF
+
+grep -qF 'replication replicator' "$PGDATA/pg_hba.conf" || \
+  echo "host replication replicator 0.0.0.0/0 md5" >> "$PGDATA/pg_hba.conf"
+
+psql -U "$POSTGRES_USER" -c "SELECT pg_reload_conf();"
+
+echo "Primary replication complete"
