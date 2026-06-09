0e606c19e3c5188883f7b8e54c37e7292381afad
Muhammad Hasan <hasanamir8901@gmail.com>
2026-06-10 01:29:39 +0500

    change type casting of returned value of breaker from <T> to Awaited<T>

--- src/config/opossum-circuit-Breaker/redisBreaker.opossum.ts
+++ src/config/opossum-circuit-Breaker/redisBreaker.opossum.ts
@@ -2,7 +2,7 @@ import breaker from "./index.opossum.js";
 
 export async function safeRedis<T>(fn: () => Promise<T>, fallback: T | null = null): Promise<T | null> {
   try {
-    return await breaker.fire(fn) as T
+    return await breaker.fire(fn) as Awaited<T>
   } catch{
     return fallback
   }
