af847ce1cfef167840485ef0726e6f5139b6447e
Muhammad Hasan <hasanamir8901@gmail.com>
2026-06-14 06:22:17 +0500

    initialize the queue for click count scheduler

---
+++ src/analytics/analytics.queue.ts
@@ -0,0 +1,6 @@
+import redis from "../config/redis/index.redis.js";
+import { Queue } from "bullmq";
+
+export const clickCountScheduler = new Queue("click-count-scheduler", {
+  connection: redis,
+});
