00a6d6085109b238d327da2d73a58e048c733764
Muhammad Hasan <hasanamir8901@gmail.com>
2026-06-15 05:32:26 +0500

    add a analytics worker layer and initialize analytics worker that uses service layer for completing job.

--- src/analytics/analytics.worker.ts
+++ src/analytics/analytics.worker.ts
@@ -1,36 +1,56 @@
-import { clickCountScheduler } from "./analytics.queue.js";
 import { Worker } from "bullmq";
-import logger from "../config/pino-logging/index.pino.js";
-import { flushClickCount } from "./analytics.service.js";
 import redis from "../config/redis/index.redis.js";
+import { parseReferrer } from "./parsers/referrer.parsers.js";
+import { getLocation } from "./parsers/geo.parsers.js";
+import { parseDevice } from "./parsers/device.parsers.js";
+import {
+  incr_click_counts,
+  insert_into_url_clicks,
+} from "./analytics.service.js";
 
-const FLUSH_INTERVAL_MS = 60_000;
-
-await clickCountScheduler.upsertJobScheduler(
-  "click-count-flush",
-  {
-    every: FLUSH_INTERVAL_MS,
-  },
-  {
-    name: "click-count-flush",
-    data: { msg: "click count flusher runs" },
-    opts: {
-      attempts: 3,
-      backoff: {
-        type: "exponential",
-        delay: 1000,
-      },
-      removeOnComplete: 10,
-      removeOnFail: true,
-    },
-  },
-);
-
-export const worker = new Worker(
-  "click-count-scheduler",
+//ananlytics worker
+export const analyticsWorker = new Worker(
+  "click",
   async (job) => {
-    logger.info(`Processing job ${job.id}, with a message: ${job.data.msg}`);
-    await flushClickCount();
+    const { short_code, url_id, ip, userAgent, referrer, clicked_at } =
+      job.data;
+
+    const [device, location, source] = await Promise.all([
+      parseDevice(userAgent),
+      getLocation(ip),
+      parseReferrer(referrer),
+    ]);
+
+    const isBot = device?.isBot ?? false;
+
+    await Promise.all([
+      insert_into_url_clicks({
+        ip,
+        url_id,
+        short_code,
+        clicked_at,
+        country_code: location.countryCode,
+        country_name: location.countryName,
+        city: location.city,
+        region: location.region,
+        latitude: location.latitude,
+        longitude: location.longitude,
+        timezone: location.timezone,
+        device_type: device?.deviceType,
+        os_name: device?.OSName,
+        browser_name: device?.browserName,
+        browser_version: device?.browserVersion,
+        is_bot: isBot,
+        referrer_url: source.referrerURL,
+        referrer_domain: source.domain,
+        referrer_type: source.type,
+        referrer_name: source.name,
+      }),
+      incr_click_counts(short_code, location.countryCode, isBot),
+    ]);
   },
-  { connection: redis, concurrency: 1 },
+  {
+    connection: redis,
+    concurrency: 20,
+  },
 );
