53e9e5bb89b4df28208f7956affee5c76d73b4d6
Muhammad Hasan <hasanamir8901@gmail.com>
2026-06-19 06:12:12 +0500

    add analytics controller for getting all analytics for a specific short url

---
+++ src/controllers/analytics.controller.ts
@@ -0,0 +1,38 @@
+import type { Request, Response } from "express";
+import { getDataForDashboard } from "../services/analytics.service.js";
+import { getUrlId } from "../services/url.service.js";
+
+export const getDashBoardData = async (req: Request, res: Response) => {
+  const shortCode = req.params.shortCode;
+
+  //validate param: short code
+  if (!shortCode) {
+    return res.status(400).json({
+      status: 400,
+      success: false,
+      msg: "short code is not available",
+    });
+  }
+
+  const id = await getUrlId(String(shortCode), req.id);
+  if (!id) {
+    return res.status(400).json({
+      status: 404,
+      success: false,
+      msg: "short url is not available or got deleted",
+    });
+  }
+
+  const result = await getDataForDashboard(
+    id.data as string,
+    String(shortCode),
+    { rangeInDays: 30 },
+  );
+
+  return res.status(400).json({
+    status: 404,
+    success: false,
+    data: result,
+    msg: "analytics fetched",
+  });
+};
