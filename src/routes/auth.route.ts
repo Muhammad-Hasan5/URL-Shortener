f7d3cf85fc8d2febdaed5f96b3e6fa2070d50850
Muhammad Hasan <hasanamir8901@gmail.com>
2026-07-14 02:44:18 +0500

    add validation layer in  auth routes

--- src/routes/auth.route.ts
+++ src/routes/auth.route.ts
@@ -13,22 +13,32 @@ import {
   verifyEmail,
 } from "../controllers/auth.controller.js";
 import { verifyJWT } from "../middlewares/auth.middleware.js";
+import { validateRequest } from "../middlewares/validate.middleware.js";
+import { registerUserObject, loginSchemaObject, incomingPassword, incomingEmail, changePasswordObject, resetForgotPasswordObject} from "../@types/auth/index.types.js";
 
 const router = Router();
 
 // Public routes
-router.post("/register", registerUser);
-router.post("/login", loginUser);
+router.post("/register", validateRequest(registerUserObject), registerUser);
+router.post("/login", validateRequest(loginSchemaObject), loginUser);
 router.post("/refresh-token", refreshAccessToken);
-router.get("/verify-email", verifyEmail);
+router.get("/verify-email", validateRequest(incomingPassword), verifyEmail);
 router.post("/resend-email-verification", resendEmailVerification);
-router.post("/reset-password-request", resetPasswordRequest);
-router.post("/reset-forgot-password", resetForgotPassword);
-
-// Protected routes (require a valid accessToken)
+router.post(
+  "/reset-password-request",
+  validateRequest(incomingEmail),
+  resetPasswordRequest,
+);
+router.post("/reset-forgot-password", validateRequest(resetForgotPasswordObject), resetForgotPassword);
+//protected
 router.post("/logout", verifyJWT, logoutUser);
 router.get("/me", verifyJWT, getCurrentUser);
-router.delete("/me", verifyJWT, deleteUser);
-router.post("/change-password", verifyJWT, changePassword);
+router.delete("/me", verifyJWT, validateRequest(incomingPassword), deleteUser);
+router.post(
+  "/change-password",
+  verifyJWT,
+  validateRequest(changePasswordObject),
+  changePassword,
+);
 
 export default router;
