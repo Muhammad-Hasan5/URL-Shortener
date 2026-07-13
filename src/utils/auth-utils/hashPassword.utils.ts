7a1b716248d6165508bcce84e003c2cafb32b22b
Muhammad Hasan <hasanamir8901@gmail.com>
2026-07-14 00:16:33 +0500

    add methods for verifying and  hashing password using argon2

---
+++ src/utils/auth-utils/hashPassword.utils.ts
@@ -0,0 +1,25 @@
+import * as argon2 from "argon2";
+import logger from "../../config/pino-logging/index.pino.js";
+
+export const hashPassword = async (password: string): Promise<string> => {
+  try {
+    const hashedPassword = await argon2.hash(password);
+    return hashedPassword;
+  } catch (error: any) {
+    logger.info("unable to hash password", error);
+    throw new Error("unable to hash password", error);
+  }
+};
+
+export const verifyPassword = async (
+  hashedPass: string,
+  password: string,
+): Promise<boolean> => {
+  try {
+    const valid = await argon2.verify(hashedPass, password);
+    return valid;
+  } catch (error: any) {
+    logger.info("unable to verify password", error);
+    throw new Error("unable to verify hashed password", error);
+  }
+};
