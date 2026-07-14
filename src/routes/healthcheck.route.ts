a4eb2e63e7379f8c59418043d0a0882cff9240e8
Muhammad Hasan <hasanamir8901@gmail.com>
2026-07-15 04:07:30 +0500

    refactor  healthcheck routes

--- src/routes/healthcheck.route.ts
+++ src/routes/healthcheck.route.ts
@@ -1,10 +1,14 @@
 import { Router } from "express";
-import { apiMetrics, serverHealthCheck, serverReadyCheck } from "../controllers/healthcheck.controller.js";
+import {
+  apiMetrics,
+  serverHealthCheck,
+  serverReadyCheck,
+} from "../controllers/healthcheck.controller.js";
 
-const router = Router()
+const router = Router();
 
 router.route("/api/v1/health/live").get(serverHealthCheck);
 router.route("/api/v1/health/ready").get(serverReadyCheck);
 router.route("/api/v1/metrics").get(apiMetrics);
 
-export default router
+export default router;
