6dca2401bfa7a1cf7620c6156b3dc78e7ab12c67
Muhammad Hasan <hasanamir8901@gmail.com>
2026-07-15 04:06:55 +0500

    adda auth and rate limit middleware in analytics routes

--- src/routes/analytics.route.ts
+++ src/routes/analytics.route.ts
@@ -2,11 +2,13 @@ import { Router } from "express";
 import { getDashBoardData } from "../controllers/analytics.controller.js";
 import { requestLogger } from "../middlewares/requestLogger.middleware.js";
 import { logRequestID } from "../middlewares/logRequestID.middleware.js";
+import { verifyJWT } from "../middlewares/auth.middleware.js";
+import limiter from "../middlewares/rateLimit.middleware.js";
 
 const router = Router();
 
 router
-  .route("/analytics/:shortCode")
-  .get(logRequestID, requestLogger, getDashBoardData);
+  .route("api/v1/analytics/:shortCode")
+  .get(limiter, verifyJWT, logRequestID, requestLogger, getDashBoardData);
 
 export default router;
