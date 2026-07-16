cd9b2742b94cf4a332d37ef9a5049907a43dc8a5
Muhammad Hasan <hasanamir8901@gmail.com>
2026-07-16 07:25:30 +0500

    remove non-required logginf and correct import paths

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,7 @@
 import app from "./app.js";
 import { primaryPool, replicaPool } from "./db/pools.db.js";
 import redis from "./config/redis/index.redis.js";
-import logger from "./config/pino-logging/index.pino.js";
+import logger from "./observability/pino-logging/index.pino.js";
 import env from "./config/env.js";
 import { SnowflakeGenerator } from "./utils/snowflakeID-utils/snowflakeID.utils.js";
 import { claimMachineID } from "./utils/snowflakeID-utils/machineIdLease.utils.js";
@@ -9,19 +9,6 @@ import { analyticsQueue } from "./analytics/analytics.queue.js";
 import { analyticsWorker } from "./analytics/analytics.worker.js";
 import { aggregationQueue } from "./analytics/aggregation/queue.aggregation.js";
 import { aggregationWorker } from "./analytics/aggregation/scheduler.aggregation.js";
-
-/*
-logger.info({
-  PG_CONNECTION_STRING: env.PG_CONNECTION_STRING,
-  REDIS_URL: env.REDIS_URL,
-  PORT: env.PORT,
-  LOG_LEVEL: env.LOG_LEVEL,
-  RATE_LIMIT_MAX_REQUESTS: env.RATE_LIMIT_MAX_REQUESTS,
-  RATE_LIMIT_WINDOWSMS: env.RATE_LIMIT_WINDOWSMS,
-  NODE_ENV: env.NODE_ENV,
-  BASE_URL: env.BASE_URL,
-});
-*/
 
 const { machineId, stopRenewal } = await claimMachineID(redis);
 console.log(machineId);
