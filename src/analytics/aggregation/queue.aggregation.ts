bd172d7ea4d0fa14310d9dab4fde007a61db297e
Muhammad Hasan <hasanamir8901@gmail.com>
2026-07-16 10:27:55 +0500

    fix: update connection reference in aggregation queue to use bullConnection

--- src/analytics/aggregation/queue.aggregation.ts
+++ src/analytics/aggregation/queue.aggregation.ts
@@ -1,6 +1,6 @@
-import redis from "../../config/redis/index.redis.js";
+import { bullConnection } from "../../config/redis/index.redis.js";
 import { Queue } from "bullmq";
 
 export const aggregationQueue = new Queue("aggregation", {
-  connection: redis,
+  connection: bullConnection,
 });
