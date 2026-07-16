1072069a9c936f726e5c23b864bf6a7f5dd7afc6
Muhammad Hasan <hasanamir8901@gmail.com>
2026-07-16 13:10:04 +0500

    fix: remove register parameter from collectDefaultMetrics in Prometheus configuration

--- src/observability/prometheus-metrics/index.prometheus.ts
+++ src/observability/prometheus-metrics/index.prometheus.ts
@@ -3,7 +3,7 @@ import client from "prom-client";
 const Registry = client.Registry;
 const register = new Registry();
 
-client.collectDefaultMetrics({ register });
+client.collectDefaultMetrics();
 
 export const requestDuration = new client.Histogram({
   name: "http_request_duration_seconds",
