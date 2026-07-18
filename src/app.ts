f341c155e4c8d40568b1a0ac7b1c9c02075d5262
Muhammad Hasan <hasanamir8901@gmail.com>
2026-07-18 09:21:23 +0500

    resolve express.json() middleware issue for validating json packages

--- src/app.ts
+++ src/app.ts
@@ -8,10 +8,9 @@ import { requestLogger } from "./middlewares/requestLogger.middleware.js";
 
 const app = express();
 
+app.use(express.json());
 app.use(requestLogger)
 app.use(cookieParser())
-
-app.use(express.json({ limit: "5mb" }));
 app.use(express.urlencoded({ limit: "5mb", extended: true }));
 
 app.get("/", (req, res) => {
