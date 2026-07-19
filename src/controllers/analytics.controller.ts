c47d240bd758244602b8f4dd25a9e2d95ccd6a7a
Muhammad Hasan <hasanamir8901@gmail.com>
2026-07-20 03:01:11 +0500

    fix: refactor getDashBoardData to use asyncHandler for improved error handling

--- src/controllers/analytics.controller.ts
+++ src/controllers/analytics.controller.ts
@@ -1,41 +1,43 @@
 import type { Request, Response } from "express";
 import { getDataForDashboard } from "../services/analytics.service.js";
 import { getUrlId } from "../services/url.service.js";
+import { asyncHandler } from "../utils/asyncHandler.utils.js";
 
-export const getDashBoardData = async (req: Request, res: Response) => {
-  const userId = req.user?.id
-  const shortCode = req.params.shortCode;
+export const getDashBoardData = asyncHandler(
+  async (req: Request, res: Response) => {
+    const userId = req.user?.id;
+    const shortCode = req.params.shortCode;
 
-  //validate param: short code
-  if (!shortCode) {
-    return res.status(400).json({
-      status: 400,
-      success: false,
-      msg: "short code is not available",
-    });
-  }
+    //validate param: short code
+    if (!shortCode) {
+      return res.status(400).json({
+        status: 400,
+        success: false,
+        msg: "short code is not available",
+      });
+    }
 
-  const id = await getUrlId(String(shortCode), req.id, userId!);
-  if (!id) {
+    const id = await getUrlId(String(shortCode), req.id, userId!);
+    if (!id) {
+      return res.status(400).json({
+        status: 404,
+        success: false,
+        msg: "short url is not available or got deleted",
+      });
+    }
+
+    const result = await getDataForDashboard(
+      id.data as string,
+      String(shortCode),
+      userId!,
+      { rangeInDays: 30 },
+    );
+
     return res.status(400).json({
       status: 404,
       success: false,
-      msg: "short url is not available or got deleted",
+      data: result,
+      msg: "analytics fetched",
     });
-  }
-
-  const result = await getDataForDashboard(
-
-    id.data as string,
-    String(shortCode),
-    userId!,
-    { rangeInDays: 30 },
-  );
-
-  return res.status(400).json({
-    status: 404,
-    success: false,
-    data: result,
-    msg: "analytics fetched",
-  });
-};
+  },
+);
