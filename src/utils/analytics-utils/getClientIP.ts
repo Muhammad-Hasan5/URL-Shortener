85f69b33266ac3ebadd0853c60a8c68e9a057b83
Muhammad Hasan <hasanamir8901@gmail.com>
2026-07-21 10:30:07 +0500

    fix: improve getClientIp function to handle multiple forwarded IPs correctly

--- src/utils/analytics-utils/getClientIP.ts
+++ src/utils/analytics-utils/getClientIP.ts
@@ -3,7 +3,8 @@ import type { Request } from "express";
 export function getClientIp(req: Request){
     const forwarded = req.headers["x-forwarded-for"]
     if(forwarded){
-        return forwarded[0]?.trim()
+        const value = Array.isArray(forwarded) ? forwarded[0] : forwarded;
+        return value?.split(",")[0]?.trim();
     }
     return req.socket.remoteAddress
 }
