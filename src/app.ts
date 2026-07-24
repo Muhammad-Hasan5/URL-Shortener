593c2fe2769ad33a89a546e6ef163112e70f90e0
Muhammad Hasan <hasanamir8901@gmail.com>
2026-07-25 01:32:45 +0500

    refactor: clean up app.ts by organizing imports and simplifying JSON middleware setup

--- src/app.ts
+++ src/app.ts
@@ -2,39 +2,30 @@ import express from "express";
 import urlRouter from "./routes/url.route.js";
 import healthCheckRouter from "./routes/healthcheck.route.js";
 import analyticsRouter from "./routes/analytics.route.js";
-import authrouter from "./routes/auth.route.js"
-import cookieParser from "cookie-parser"
+import authrouter from "./routes/auth.route.js";
+import swaggerRouter from "./docs/swagger.js"
+import cookieParser from "cookie-parser";
 import { requestLogger } from "./middlewares/requestLogger.middleware.js";
 
 const app = express();
 
-app.set("trust proxy", 1);
-
-app.use(
-  express.json({
-    type: (req) => {
-      const contentType = req.headers["content-type"];
-      const value = Array.isArray(contentType) ? contentType[0] : contentType;
-      return !value || /application\/(?:[\w.-]+\+)?json/i.test(value);
-    },
-  }),
-);
-
-app.use(requestLogger)
-app.use(cookieParser())
+app.use(express.json());
+app.use(requestLogger);
+app.use(cookieParser());
 app.use(express.urlencoded({ limit: "5mb", extended: true }));
 
 app.get("/", (req, res) => {
   res.send("welcome to URLY!");
 });
 
-
-app.use("/", authrouter)
-
+//attaching swagger docs to app
+app.use("/api-docs", swaggerRouter);
+//attaching healthcheck-router to app
+app.use("/", authrouter);
 app.use("/", healthCheckRouter);
-
+//attaching url-router to app
 app.use("/", urlRouter);
-
+//attaching analytics router to app
 app.use("/", analyticsRouter);
 
 export default app;
